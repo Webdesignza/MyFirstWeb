947b5552ea70f1e9dfa20cf022927348ed7e594f
Jaco Hoffman <jaco.hoffman@gmail.com>
2024-05-15 10:46:50 +0200

    Added Mobirise Code

--- assets/scripts/OurScrips.js
+++ assets/scripts/OurScrips.js
@@ -3,6 +3,9 @@
  let nameAndAge = [];
  let imgStickman = document.getElementById("stickman");
  let posLeft = 0;
+ let posHeight = 150;
+ let imgCount = 0;
+ let heightCount = 0;
 
 
 function add(name, age){
@@ -52,15 +55,45 @@ function run(){
 
 function animateMan(){    
 
+
+    switch(imgCount){
+        case  0 :
+            imgStickman.src =  "assets/images/stickman.png";
+            break;
+        case 5 :
+            imgStickman.src = "assets/images/stickman1.png";
+            break;
+        case 10 :
+            imgStickman.src = "assets/images/stickman2.png";      
+            break; 
+
+    }
+    
     imgStickman.style.display = "block";
     imgStickman.style.left = posLeft + "px";
-
-    posLeft += 7;
-
+    imgStickman.style.height = posHeight + "px";
     
+    posLeft += 7;
+    if (heightCount == 2){
+        posHeight -= 1;
+        heightCount = 0;
+    }
+    imgCount++;
+    heightCount++;
+    
+    if(imgCount > 10){
+        imgCount = 0;
+    }
 
     if(posLeft < 1300){
         requestAnimationFrame(animateMan);
+    }
+    else{
+        posLeft = 0;
+        posHeight = 150;
+        imgStickman.style.display = "none";
+        imgStickman.style.left = 0;
+
     }
 
 }
